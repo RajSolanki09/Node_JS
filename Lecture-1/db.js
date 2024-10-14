786de8ada0cda5d15a1efecd1d6d3842ef033d29
3811 Raj Solanki <solankiraj9642@gmail.com>
2024-10-14 09:57:07 +0530

    Node

--- Lecture-1/db.js
+++ Lecture-1/db.js
@@ -2,7 +2,7 @@
 const { default: mongoose } = require('mongoose')
 const mongose = require('mongoose')
 const dbConnect = async () => {
-    await mongoose.connect("mongodb+srv:Raj_Solanki:node@cluster0.kel73.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    await mongoose.connect("mongodb+srv://Raj_Solanki:node@cluster0.kel73.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 }
 console.log("connected to db");
 module.exports = dbConnect
--- Lecture-1/index.js
+++ Lecture-1/index.js
@@ -2,11 +2,8 @@ const express = require('express');
 const dbConnect = require('./db');
 const User = require('./user.model');
 const app = express()
+app.use(express.json())
 
-app.listen(8090, () => {
-    console.log("Server is running on port 8090");
-    dbConnect
-})
 
 app.get("/", async (req, res) => {
     let data = await User.find()
@@ -17,5 +14,8 @@ app.post("/", async (req, res) => {
     let data = await User.create(req.body)
     res.send(data)
 })
-
-app.use(express.json())
+app.listen(8090, () => {
+    console.log("Server is running on port 8090");
+    // http://localhost:8090/
+    dbConnect()
+})
