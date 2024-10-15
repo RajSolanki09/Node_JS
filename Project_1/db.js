1a7260685303dc2da7d049107e09e2979467c834
3811 Raj Solanki <solankiraj9642@gmail.com>
2024-10-15 22:00:59 +0530

    node

---
+++ Project_1/db.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', async (req, res) => {
+    
+})
+
+app.listen(8888, () => {
+    console.log("Server is running on port 8888");
+})
