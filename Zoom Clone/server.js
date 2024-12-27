91510e0e7da01b426f8fcd7816b381f54aa290c4
3811 Raj Solanki <solankiraj9642@gmail.com>
2024-12-27 16:01:26 +0530

    node

--- Zoom Clone/server.js
+++ Zoom Clone/server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
+
+const { ExpressPeerServer } = require('peer')
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+})
+
 app.set("view engine", "ejs");
 const io = require("socket.io")(server);
 app.use(express.static("public"));
-
+app.use('/peerjs', peerServer)
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
