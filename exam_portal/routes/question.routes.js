5a2b7e333d5df667577742cc5292bf98d0093812
3811 Raj Solanki <solankiraj9642@gmail.com>
2025-01-21 13:18:22 +0530

    node

--- exam_portal/routes/question.routes.js
+++ exam_portal/routes/question.routes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const questionController = require("../controllers/question.controller");
 const { authMiddleware, checkRole } = require("../middleware/user.middleware");
 
-router.post("/", authMiddleware, checkRole(['teacher']), questionController.addQuestion);
+router.get("/", authMiddleware, questionController.getAllQuestions);
+router.get("/user/:userId", authMiddleware, questionController.getQuestionsByUser);
 router.get("/:examId", authMiddleware, questionController.getQuestions);
+router.post("/", authMiddleware, checkRole(['teacher']), questionController.addQuestion);
 router.put("/:questionId", authMiddleware, checkRole(['teacher']), questionController.updateQuestion);
 router.delete("/:questionId", authMiddleware, checkRole(['teacher']), questionController.deleteQuestion);
+
 
 module.exports = router;
